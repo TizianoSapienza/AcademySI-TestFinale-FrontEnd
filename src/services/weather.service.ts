cddc9e59e6bf6a36b1f5c0463693db214473eb22
Tiziano Sapienza <tizi.sapienza@gmail.com>
2024-07-25 10:21:38 +0200

    Error handling in WeatherService

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environments';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 import { WeatherResponse } from '../interfaces/weather-Response';
 import { WeatherPostDto } from '../interfaces/weatherPostDto';
 
@@ -24,11 +24,26 @@ export class WeatherService {
 
   getWeather(city: string): Observable<WeatherResponse> {
     const headers = this.getHeaders();
-    return this.http.get<WeatherResponse>(`${this.apiUrl}/weather/current?city=${city}`, { headers });
+    return this.http.get<WeatherResponse>(`${this.apiUrl}/weather/current?city=${city}`, { headers }).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   saveWeather(weather: WeatherPostDto): Observable<any> {
     const headers = this.getHeaders();
-    return this.http.post<any>(`${this.apiUrl}/weather/save`, weather, { headers });
+    return this.http.post<any>(`${this.apiUrl}/weather/save`, weather, { headers }).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
